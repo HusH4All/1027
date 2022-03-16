e07c9979e09964d105138e5a8de50ad5ec1e63de
HusH4All <al394823@uji.es>
2022-03-16 09:55:24 +0100

    oleeeeeeeeeeeeee

---
+++ clubesportiu/src/main/java/es/uji/ei1027/clubesportiu/controller/MainController.java
@@ -0,0 +1,14 @@
+package es.uji.ei1027.clubesportiu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String welcome(){
+        return "index";
+    }
+}
